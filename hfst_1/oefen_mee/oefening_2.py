b66252b21faa00d58a2ca36e848b9bb734d15eae
pieter <pieter.swinnen.4@gmail.com>
2022-09-22 11:18:40 +0200

    update

--- hfst_1/oefen_mee/oefening_2.py
+++ hfst_1/oefen_mee/oefening_2.py
@@ -9,3 +9,9 @@ boodschappen_dict = {'appel': 9, 'doerian': 3, 'banaan': 3, 'kers': 3, 'mango': 
 """
 
 boodschappen_dict = {}
+
+for fruit in boodschappen_lijst:
+    if fruit not in boodschappen_dict:
+        boodschappen_dict[fruit] = boodschappen_lijst.count(fruit)
+
+print(boodschappen_dict)
--- hfst_1/oefen_mee/oefening_5.py
+++ hfst_1/oefen_mee/oefening_5.py
@@ -6,5 +6,9 @@ Voorbeeld:
 
 Tip: je hebt al in de reeksen gezien hoe een woord om te keren.
 """
-
+omgekeerd_dict = {}
 zin = input("Geef een zin op: ")
+woorden = zin.split()
+for woord in woorden:
+    omgekeerd_dict[woord] = woord[::-1]
+print(omgekeerd_dict)
