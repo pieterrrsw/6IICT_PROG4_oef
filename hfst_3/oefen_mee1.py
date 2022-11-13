c48305e1126fad6ac5e03852dbb0f4a9cd3ff886
HendrikClijstersMosa <hendrik.clijsters@mosa-rt.be>
2022-11-13 16:32:09 +0100

    Oefen mee hoofdstuk 3

---
+++ hfst_3/oefen_mee1.py
@@ -0,0 +1,4 @@
+getal = int( input("Geef een getal in: ") )
+deling_10 = 10/getal
+
+print(f"10 gedeeld door {getal} is gelijk aan {deling_10}")
---
+++ hfst_3/oefen_mee7.py
@@ -0,0 +1,9 @@
+""" Niveau 1 """
+num_lijst = [ 100, 101, 0, "103", 104 ]
+index = int( input( "Geef een index: " ) )
+
+""" Niveau 2 (haal uit commentaar) """
+# print( f"100 / {num_lijst[index]} = {100/num_lijst[index]}" ) 
+
+""" Niveau 3 (haal uit commentaar) """
+# print( "Geldig getal als index opgegeven." )
