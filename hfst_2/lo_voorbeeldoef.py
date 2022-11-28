6e9e66c366aa5bea79aadb6254d0181910410d0f
HendrikClijstersMosa <hendrik.clijsters@mosa-rt.be>
2022-11-28 12:17:30 +0100

    Voorbeeldoefening LO

---
+++ hfst_2/lo_voorbeeldoef.py
@@ -0,0 +1,37 @@
+""" Algemeen:
+Schrijf bij iedere regel code commentaar 
+De code mag geen foutmeldingen geven
+    Opgelet! Code in commentaar wordt niet bekeken.
+"""
+
+""" 
+lo_voorbeeldoefening.json bevat info over het schaakstuk "toren".
+"""
+
+""" STAP 1:
+laad lo_voorbeeldoefening.json in Python. Zet deze dictionary in een variabele.
+
+Lukt dit niet? Dan mag je de dictionary rechtstreeks hieronder plakken.
+               Je krijgt dan wel geen punten voor dit onderdeel.
+"""
+
+""" STAP 2:
+print volgende zaken over de toren:
+    - De naam zelf (toren)
+    - Hoeveel pionnen deze waard is (5)
+    - 1 van de beweging die het kan maken (bvb. boven)
+
+Je moet deze info uit de dictionary halen (dus niet manueel invullen).
+"""
+
+""" STAP 3:
+Voeg volgende zaken toe aan de dictionary (links staat de key, rechts de value):
+    - engelse_naam: rook
+    - andere_namen: ["Toring", "Torra", "Rukhkh", "Top"]
+
+"""
+
+""" STAP 4:
+Schrijf de gewijzigde dictionary weg naar een NIEUW JSON-bestand.
+Bijvoorbeeld lo_voorbeeldresultaat.json .
+"""
